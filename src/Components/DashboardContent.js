9b7771bd031dcfe4d8a56f51cf2fa4b310354d2f
RatiMathur <rmathur18@gmail.com>
2023-03-14 21:16:52 -0400

    Dashboard UI changes

--- src/Components/DashboardContent.js
+++ src/Components/DashboardContent.js
@@ -18,7 +18,34 @@ export default function DashboardContent() {
           />
         </span>
       </h1>
-      <img src={Book} />
+      <div style={{ display: "grid" }}>
+        <img
+          src={Book}
+          style={{
+            width: "80vw",
+            height: "50vh",
+          }}
+        />
+        <p>
+          Enchanting the mind one book at a time is our core focus at KH Book
+          Outlet. It’s the passion behind everything we do. We believe we’re not
+          just selling books; we’re impacting the lives of millions of people,
+          both locally and across the globe, by making books more accessible to
+          everyone. With reading comes learning, and the more we learn the
+          greater the impact we can all make.{" "}
+        </p>
+
+        <p>
+          While we’re not the type of company who likes to stand on the rooftop
+          and shout out the ways we support our communities, we are proud of the
+          number of people we’ve been able to impact through our contributions
+          and book donations to literacy and education programs. Below are just
+          a few of the organizations that we’ve supported along the way. Each
+          month, we review dozens of donation requests from incredible
+          individuals and organizations worldwide doing their part in Enchanting
+          the Minds within their community.
+        </p>
+      </div>
     </div>
   );
 }
